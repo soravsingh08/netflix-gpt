1571ff947daee087a9bd6f42385f310e2001955d
sorav08 <rahulrakwal3@gmail.com>
2026-01-17 16:36:57 +0530

    Add Netflix GPT project files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">This is out neflix gpt</h1>
+    </div>
+  )
+}
+
+export default App
